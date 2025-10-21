5f95461a675d77794024dc2ee4f5b3e07de23a90
saatvik-10 <saatvikmadan529@gmail.com>
2025-10-21 14:59:37 +0530

    added server

---
+++ monorepo_dc/apps/server/index.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import { prismaClient } from 'db/client'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prismaClient.user.findMany()
+        res.json({users})
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+app.post('/user', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.status(400).json("Credentials are required")
+        return
+    }
+
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                username,
+                password
+            }
+        })
+        console.log('User created:', user)
+        res.status(201).json(user)
+    } catch (err) {
+        console.error('Error creating user:', err)
+        res.status(500).json(err)
+    }
+})
+
+app.listen(8080)
